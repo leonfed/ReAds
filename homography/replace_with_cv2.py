77a2ebc4a812c0394c799f6cefd2317ac8b16747
fedleonid <fedleonid@yandex-team.ru>
2021-03-29 09:53:52 +0300

    improve replace_with_cv2.py

--- homography/replace_with_cv2.py
+++ homography/replace_with_cv2.py
@@ -1,36 +1,44 @@
+import random
+
 import cv2
 import numpy as np
+import pandas
 
-# synthetic_16
-# Координаты контура
-# [[[981  54]]
-#  [[701  86]]
-#  [[683 481]]
-#  [[962 502]]]
+all_corners = pandas.read_csv('corners.csv').values
 
+vertical_images_name = ['41', '42']
+horizontal_images_name = ['7']
 
-# Read source image.
-img_src = cv2.imread('for_replace.jpg')
-print(img_src.shape)
-# Four corners of the book in source image
-corners_src = np.array([[0, 0], [img_src.shape[1], 0], [img_src.shape[1], img_src.shape[0]], [0, img_src.shape[0]]])
+for corners in all_corners:
+    print('Corners: %s' % corners)
+    [filename, x1, y1, x2, y2, x3, y3, x4, y4] = corners
 
-# Read destination image.
-img_dst = cv2.imread('synthetic_16.jpg')
-# Four corners of the book in destination image.
-corners_dst = np.array([[701, 86], [981, 54], [962, 502], [683, 481]])
+    # Read banner
+    if abs(x2 - x1) + abs(x3 - x2) < abs(y2 - y1) + abs(y3 - y2):
+        banner_name = random.choice(vertical_images_name)
+    else:
+        banner_name = random.choice(horizontal_images_name)
+    print("Banner name: %s" % banner_name)
+    img_banner = cv2.imread('../images/%s.jpg' % banner_name)
+    # Four corners of the book in banner image
+    corners_banner = np.array([[0, 0], [img_banner.shape[1], 0], [img_banner.shape[1], img_banner.shape[0]], [0, img_banner.shape[0]]])
 
-# Calculate Homography
-h, status = cv2.findHomography(corners_src, corners_dst)
+    # Read destination image.
+    img_dst = cv2.imread('source/%s.jpg' % filename)
+    # Four corners of the book in destination image.
+    corners_dst = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
 
-# Warp source image to destination based on homography
-img_out = cv2.warpPerspective(img_src, h, (img_dst.shape[1], img_dst.shape[0]))
+    # Calculate Homography
+    h, status = cv2.findHomography(corners_banner, corners_dst)
 
-# Удалить баннер, который надо заменить
-mask = np.ones(img_dst.shape[:2], dtype="uint8") * 255
-cv2.drawContours(mask, [corners_dst], -1, 0, -1)
-img_dst = cv2.bitwise_and(img_dst, img_dst, mask=mask)
+    # Warp source image to destination based on homography
+    img_out = cv2.warpPerspective(img_banner, h, (img_dst.shape[1], img_dst.shape[0]))
 
-# Смержить изображения
-img_res = cv2.addWeighted(img_dst, 1, img_out, 1, 0.0)
-cv2.imwrite("result.jpg", img_res)
+    # Удалить баннер, который надо заменить
+    mask = np.ones(img_dst.shape[:2], dtype="uint8") * 255
+    cv2.drawContours(mask, [corners_dst], -1, 0, -1)
+    img_dst = cv2.bitwise_and(img_dst, img_dst, mask=mask)
+
+    # Смержить изображения
+    img_res = cv2.addWeighted(img_dst, 1, img_out, 1, 0.0)
+    cv2.imwrite("cv2_results/%s.jpg" % filename, img_res)
