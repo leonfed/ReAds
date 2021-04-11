6f88d4471c100ad53a592187c8bd17ef4502e9d7
fedleonid <fedleonid@yandex-team.ru>
2021-04-11 21:12:49 +0300

    refactor json annotation for detectron

---
+++ detectron/create_detectron_data.py
@@ -0,0 +1,68 @@
+import os
+import random
+import shutil
+
+import pandas
+
+# удалить содержимое папок
+shutil.rmtree('detectron_test_data')
+os.makedirs('detectron_test_data')
+shutil.rmtree('detectron_train_data')
+os.makedirs('detectron_train_data')
+
+# прочитать синтетические фото
+synthetic_data = pandas.read_csv('../generation/banner/annotation.csv').values
+random.shuffle(synthetic_data)
+
+# разделить на тестовыей и тренировычный
+to_test_data_count = 30
+test_synthetic_data = synthetic_data[:to_test_data_count]
+train_synthetic_data = synthetic_data[to_test_data_count:]
+
+synthetic_index = 0
+
+# файлы для запоминания того, где располежены углы баннеров
+test_synthetic_csv = open('detectron_test_data/synthetic_ground_truth.csv', 'w')
+test_synthetic_csv.write("filename,x1,y1,x2,y2,x3,y3,x4,y4\n")
+train_synthetic_csv = open('detectron_train_data/synthetic_ground_truth.csv', 'w')
+train_synthetic_csv.write("filename,x1,y1,x2,y2,x3,y3,x4,y4\n")
+
+# скопировать датасеты
+for current_data in test_synthetic_data:
+    [screenshot, room, wall_index, image, x1, y1, x2, y2, x3, y3, x4, y4] = current_data
+    filename = 'synthetic_' + str(synthetic_index)
+    synthetic_index += 1
+    shutil.copy('../generation/banner/%s.png' % screenshot, 'detectron_test_data/%s.jpg' % filename)
+    test_synthetic_csv.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (filename, x1, y1, x2, y2, x3, y3, x4, y4))
+for current_data in train_synthetic_data:
+    [screenshot, room, wall_index, image, x1, y1, x2, y2, x3, y3, x4, y4] = current_data
+    filename = 'synthetic_' + str(synthetic_index)
+    synthetic_index += 1
+    shutil.copy('../generation/banner/%s.png' % screenshot, 'detectron_train_data/%s.jpg' % filename)
+    train_synthetic_csv.write("%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (filename, x1, y1, x2, y2, x3, y3, x4, y4))
+
+print("Synthetic - OK")
+
+# прочитать реальные фото
+annotations_files = os.listdir('/home/fedleonid/Study/diploma/annotations_photos')
+random.shuffle(annotations_files)
+
+# указать те, которые точно нужно поместить в тестовый датасет
+prefixes_for_test = ['951', '952', '953', '954', '957']
+test_real_data = list(filter(lambda x: prefixes_for_test.__contains__(x.split('.')[0]), annotations_files))
+annotations_files = list(filter(lambda x: not prefixes_for_test.__contains__(x.split('.')[0]), annotations_files))
+
+# указать сколько файлов еще закинуть в тестовый датасет
+to_test_data_count = 65
+test_real_data = test_real_data + annotations_files[:to_test_data_count]
+train_real_data = annotations_files[to_test_data_count:]
+
+for file in test_real_data:
+    filename = file.split('.')[0]
+    shutil.copy('/home/fedleonid/Study/diploma/photos/%s.jpg' % filename, 'detectron_test_data/%s.jpg' % filename)
+
+for file in train_real_data:
+    filename = file.split('.')[0]
+    shutil.copy('/home/fedleonid/Study/diploma/photos/%s.jpg' % filename, 'detectron_train_data/%s.jpg' % filename)
+
+print("Real - OK")
---
+++ detectron/jsonAnnotation.py
@@ -0,0 +1,66 @@
+import random
+import shutil
+
+import pandas
+import json
+import os
+import xml.etree.ElementTree as ET
+
+result = {}
+
+files = os.listdir('detectron_train_data')
+
+# отделяем реальные фото от синтететических
+real_files = list(filter(lambda x: not x.startswith('synthetic_'), files))
+synthetic_data = pandas.read_csv('detectron_train_data/synthetic_ground_truth.csv').values
+
+# обрабатываем синтетические фото
+for i in range(len(synthetic_data)):
+    current_data = synthetic_data[i]
+    [filename, x1, y1, x2, y2, x3, y3, x4, y4] = current_data
+
+    polygon = {"shape_attributes": {"all_points_x": [x1, x2, x3, x4, x1], "all_points_y": [y1, y2, y3, y4, y1]}}
+    current_object = {"filename": "%s.png" % filename, "regions": {"0": polygon}}
+
+    result[filename] = current_object
+
+print("Synthetic - OK")
+
+# обрабатываем реальные фото
+for file in real_files:
+    filename = file.split('.')[0]
+    tree = ET.parse('/home/fedleonid/Study/diploma/annotations_photos/' + filename + ".xml")
+
+    objects = tree.getroot().findall("object")
+
+    regions = {}
+    polygon_cnt = 0
+
+    for obj in objects:
+        xml_points = obj.find('polygon').findall('pt')
+        points_x = []
+        points_y = []
+        for xp in xml_points:
+            x = int(xp.find('x').text)
+            y = int(xp.find('y').text)
+            points_x.append(x)
+            points_y.append(y)
+
+        points_x.append(points_x[0])
+        points_y.append(points_y[0])
+
+        polygon = {"shape_attributes": {"all_points_x": points_x, "all_points_y": points_y}}
+        regions[str(polygon_cnt)] = polygon
+
+        polygon_cnt += 1
+
+    current_object = {"filename": "%s.jpg" % filename, "regions": regions}
+
+    result[filename] = current_object
+
+print("Real - OK")
+
+file_screenshots_csv = open('detectron_train_data/via_region_data.json', 'w')
+file_screenshots_csv.write(json.dumps(result))
+
+print("OK")
